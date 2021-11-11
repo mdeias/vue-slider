11b31a0688aa87acae0169197d7430d0d40e44e6
mdeias <mdeias@icloud.com>
2021-11-11 15:39:03 +0100

    adatto dom e creo un contatore

--- js/script.js
+++ js/script.js
@@ -4,7 +4,17 @@ const app = new Vue({
 
     el: '#app',
     data: {
-        images: []
+        images: [
+
+            'img/01.jpg', 'img/02.jpg', 'img/03.jpg', 'img/04.jpg', 'img/05.jpg'
+        ],
+
+        titles: [
+            'Svezia', 'Svizzera', 'Gran Bretagna', 'Germania', 'Paradise'
+        ],
+        
+        // creo la proprietà contatore per ciclare l'array
+        counter: 0
     },
 
     methods: {
